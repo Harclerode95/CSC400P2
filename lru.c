10fdf3c2d21d902aaab76ecd626ef0a9500b0ade
Harclerode95 <38046711+Harclerode95@users.noreply.github.com>
2018-04-11 17:29:53 -0400

    Create lru.c

---
+++ lru.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "time.h"
+#include "stdlib.h" //required for random()
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
+
+#define PFRAME 5
+//Assume that the program will be assigned 5 page frames
+
+void Error(char * msg){
+  printf("%s \n", msg);
+  exit(0);
+}
