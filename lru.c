b65831aa0f161cdf00cd77bb985a25cb09adbaf6
SolarisDea <38270821+SolarisDea@users.noreply.github.com>
2018-04-11 20:20:48 -0400

    Update lru.c

--- lru.c
+++ lru.c
@@ -12,3 +12,81 @@ void Error(char * msg){
   printf("%s \n", msg);
   exit(0);
 }
+
+// Check if page is in memory ( 1 = yes, 0 = no )
+int pageinmem(int * mem, int page){
+	int i;
+	for (i=0; i<PFRAME; i++)
+		if (mem[i] == page) return 1;
+  
+	return 0;
+}
+
+// Load page into memory
+void load(int * mem, int page){
+	int i;
+
+	for (i=0; i<PFRAME; i++)
+		// Is current memory slot holding page?
+		if (mem[i] == -1) {
+			// If not, store this page
+			mem[i]=page;
+			return;
+		}	
+}
+
+// List the memory
+void listmemory(int * mem){
+	int i;
+
+	for (i=0;i<PFRAME; i++)
+		// If page is in memory slot, print it
+		if (mem[i]!=-1) printf("[%c]", mem[i]);
+		// Otherwise, print -1 (no page in memory slot)
+		else printf("[-1]");
+  
+	printf("\n");
+	return;
+}
+
+
+int main(int argc, char * argv[]){
+	int i, page, toreplace, pagefault = 0, totalpage = 0;
+	int memory[PFRAME];
+	char pagereference[21]="ABCDEEDBCFJKMCBFFCMA";
+	//  char pagereference[21]="ABCDEFGHIJKLMNOPQRST";
+	
+   // Memory starts out as list of -1
+    for (i=0;i<PFRAME; i++) memory[i] = -1;
+	
+    for (i=0;i<20; i++) {
+	  // Everytime read a page
+		page = pagereference[i];  
+		printf("%c", page);
+		
+	  // Is the page in memory?
+		if (!pageinmem(memory, page)) {
+		  // If not, update pagefault (number of faults)
+			pagefault++;
+			
+		  // Is memory full?
+			if (memory[PFRAME - 1] != -1) {	
+				toreplace = (int) ((float)random()/RAND_MAX*PFRAME);
+				printf("(M)   ->%d   ", toreplace + 1);
+			  // Replace:
+				memory[toreplace] = page;
+			}
+			
+			else 
+				load(memory, page);
+				listmemory(memory);
+		}
+	  // Page exists
+		else printf("(H)\n");	
+		}
+	
+    printf("\n Total page fault rate is %f\n",(float)pagefault/20);
+
+	return 1;
+
+}
